5fd5b89ccd5edba8604d9106052390b879dcc616
premkumar01 <premkumar.k4898@gmail.com>
2020-09-04 00:12:58 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  user = {
+    id:0,
+    name:'',
+    username:'',
+    email:'',
+    address:{
+      street:'',
+      suite:'',
+      city:'',
+      zipcode:'',
+      geo:{
+        lat:'',
+        lng:''
+      }
+    },
+    phone:'',
+    website:'',
+    company:{
+      name:'',
+      catchPhrase:'',
+      bs:''
+    }
+  }
+
+  onSubmit(theForm: NgForm)
+  {
+    this.user.id=this.user.id+1;
+    this.user.name=theForm.value.mname;
+    this.user.username=theForm.value.username;
+    this.user.email=theForm.value.email;
+    this.user.address.street=theForm.value.street;
+    this.user.address.suite=theForm.value.suite;
+    this.user.address.city=theForm.value.city;
+    this.user.address.zipcode=theForm.value.zipcode;
+    this.user.address.geo.lat=theForm.value.lat;
+    this.user.address.geo.lng=theForm.value.lng;
+    this.user.phone=theForm.value.phone;
+    this.user.website=theForm.value.website;
+    this.user.company.name=theForm.value.cname;
+    this.user.company.catchPhrase=theForm.value.catchphrase;
+    this.user.company.bs=theForm.value.bs;
+
+    console.log(this.user);
+  }
 }
